20add436e4c17202d763f7ff5561c35a6a0e0918
Thomas Mechelke <tmech@tmechlabs.com>
2015-07-03 17:18:21 +0200

    Starting over with UI project

---
+++ Zeitgeist/Zeitgeist.Web.API.Tests/TimeEntryTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Zeitgeist.Web.API.Tests
+{
+    public class TimeEntryTests
+    {
+        
+        [Fact]
+        public void ShouldHaveStartTimeProperty()
+        {
+
+        }
+    }
+}
